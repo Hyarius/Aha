2113a0c4c41562f9571999dbccfca3b2aaa24c77
Jeremy BOISSY <jboissy@e2r11p11.42.fr>
2020-02-22 14:40:25 +0100

    Strange bug over opengl light

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -57,7 +57,6 @@ class c_board_widget : public c_widget
 {
 private:
 	float _zoom;
-	Vector3 _target;
 	c_camera *_camera;
 	c_engine *_engine;
 	c_mesh *_board;
@@ -67,8 +66,7 @@ public:
 	{
 		_zoom = 0.0f;
 		_camera = new c_camera(Vector3(2, 2, 2), 70.0f, 0.1f, 1000.0f);
-		_target = Vector3(0.0f, 0.0f, 0.0f);
-		_camera->look_at(_target);
+		_camera->look_at(0);
 		_engine = new c_engine();
 		_board = _engine->add_mesh(new c_mesh(0));
 		_transparent_board = _engine->add_mesh(new c_mesh(0));
@@ -108,9 +106,12 @@ public:
 			if (_engine->mesh(i)->transparency() != 1 && _engine->mesh(i)->transparency() != 0)
 				_engine->mesh(i)->render(_camera);
 
-		draw_text("Camera pos = " + _camera->pos().str(), coord, 16, 0, text_color::white);
-		draw_text("Target pos = " + _target.str(), coord + Vector2(0, 18), 16, 0, text_color::white);
-		draw_text("Zoom = " + ftoa(_zoom, 4), coord + Vector2(0, 36), 16, 0, text_color::white);
+		size_t i = 0;
+		draw_text("Camera pos = " + _camera->pos().str(), coord + Vector2(0, 16 * i++), 16, 0, text_color::white);
+		draw_text("Direction_list = " + _camera->dir_light().str(), coord + Vector2(0, 16 * i++), 16, 0, text_color::white);
+		for (size_t j = 0; j < 6; j++)
+			draw_text("Normale first voxel face [" + to_string(j) + "]= " + _engine->mesh(0)->faces(j)->normale.str(), coord + Vector2(0, 16 * i++), 16, 0, text_color::white);
+		draw_text("Zoom = " + ftoa(_zoom, 4), coord + Vector2(0, 16 * i++), 16, 0, text_color::white);
 	}
 	bool handle_mouse()
 	{
@@ -122,63 +123,45 @@ public:
 				_camera->move(_camera->forward().normalize() * (g_mouse->wheel < 0 ? 0.5f : -0.5f));
 			}
 		}
+		if (get_mouse()->get_button(mouse_button::middle) == mouse_state::down)
+		{
+			_camera->rotate(get_mouse()->rel_pos.x * g_application->fps_ratio() * 3, get_mouse()->rel_pos.y * g_application->fps_ratio() * 3);
+		}
 		return (false);
 	}
 	bool handle_keyboard()
 	{
 		Vector3 delta;
 
-		if (g_keyboard->get_key(SDL_SCANCODE_Q) == key_state::down)
-		{
-			_camera->rotate_around_point(_target, Vector3(0, 1, 0) * g_application->fps_ratio());
-			_camera->look_at(_target);
-		}
-		if (g_keyboard->get_key(SDL_SCANCODE_E) == key_state::down)
-		{
-			_camera->rotate_around_point(_target, Vector3(0, -1, 0) * g_application->fps_ratio());
-			_camera->look_at(_target);
-		}
 		if (g_keyboard->get_key(SDL_SCANCODE_SPACE) == key_state::down)
 		{
 			delta = Vector3(0.0f, 0.25f, 0.0f) * g_application->fps_ratio();
-			_target += delta;
 			_camera->move(delta);
-			_camera->look_at(_target);
 		}
 		if (g_keyboard->get_key(SDL_SCANCODE_LCTRL) == key_state::down)
 		{
 			delta = Vector3(0.0f, -0.25f, 0.0f) * g_application->fps_ratio();
-			_target += delta;
 			_camera->move(delta);
-			_camera->look_at(_target);
 		}
 		if (g_keyboard->get_key(SDL_SCANCODE_W) == key_state::down)
 		{
 			delta = _camera->forward() * Vector3(0.25f, 0.0f, 0.25f) * -g_application->fps_ratio();
-			_target += delta;
 			_camera->move(delta);
-			_camera->look_at(_target);
 		}
 		if (g_keyboard->get_key(SDL_SCANCODE_S) == key_state::down)
 		{
 			delta = _camera->forward() * Vector3(0.25f, 0.0f, 0.25f) * g_application->fps_ratio();
-			_target += delta;
 			_camera->move(delta);
-			_camera->look_at(_target);
 		}
 		if (g_keyboard->get_key(SDL_SCANCODE_A) == key_state::down)
 		{
 			delta = _camera->right() * Vector3(0.25f, 0.0f, 0.25f) * -g_application->fps_ratio();
-			_target += delta;
 			_camera->move(delta);
-			_camera->look_at(_target);
 		}
 		if (g_keyboard->get_key(SDL_SCANCODE_D) == key_state::down)
 		{
 			delta = _camera->right() * Vector3(0.25f, 0.0f, 0.25f) * g_application->fps_ratio();
-			_target += delta;
 			_camera->move(delta);
-			_camera->look_at(_target);
 		}
 		return (false);
 	}
@@ -198,7 +181,12 @@ int		main(void)
 
 	for (size_t i = 0; i < 10; i++)
 		for (size_t j = 0; j < 10; j++)
-			render.add_voxel(primitive_voxel(Vector3(i, 0.0f, j), tile, (i == 0 || i == 9 || j == 0 || j == 9 ? 1 : 0)));
+		{
+			if (i == 0 || i == 9 || j == 0 || j == 9)
+				render.add_voxel(primitive_voxel(Vector3(i, 0.0f, j), tile, 1));
+			else
+				render.add_voxel(primitive_voxel(Vector3(i, 0.0f, j), tile, 4, 0.5f));
+		}
 
 	render.board()->bake();
 	render.board()->set_texture(tile);
